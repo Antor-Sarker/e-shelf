1ee14e46ef37f101527dcceaeedcacdb2651bca2
antor sarker <antorsarker55@gmail.com>
2025-07-18 12:19:39 +0600

    feat(api): create GET api/books/publication route to fetch publication list

---
+++ src/app/api/books/publication/route.js
@@ -0,0 +1,33 @@
+import clientPromise from "../../../../../lib/mongodb";
+
+export async function GET(request) {
+  try {
+    const client = await clientPromise;
+    const db = await client.db(process.env.BOOKS_DB);
+    const books = await db.collection("products").find({}).toArray();
+    const publications = await db.collection("publications").find({}).toArray();
+
+    //add stock for every publication
+    const results = await publications.map((publication) => {
+      return {
+        ...publication,
+        stock: books.filter((book) => book.publication === publication.name)
+          .length,
+      };
+    });
+
+    return new Response(JSON.stringify(results), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ error: "failed to fetch publications" }),
+      {
+        status: 500,
+      }
+    );
+  }
+}
