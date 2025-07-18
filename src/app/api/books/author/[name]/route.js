c508b38601024756f0f86c50d9ac6897d7855439
antor sarker <antorsarker55@gmail.com>
2025-07-18 11:53:06 +0600

    feat(api): create GET api/books/category/[name] route for filter by author

---
+++ src/app/api/books/author/[name]/route.js
@@ -0,0 +1,22 @@
+import clientPromise from "../../../../../../lib/mongodb";
+
+export async function GET(request, { params }) {
+  try {
+    const { name } = await params;
+    const client = await clientPromise;
+    const db = await client.db(process.env.BOOKS_DB);
+    const books = await db
+      .collection("products")
+      .find({ author: { $regex: name, $options: "i" } })
+      .toArray();
+
+    return new Response(JSON.stringify(books), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: "failed to fetch books" }), {
+      status: 500,
+    });
+  }
+}
